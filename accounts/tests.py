c35161aff3cfbf6e4725efd0b6dea53caebbb20b
Michael Jarvis <mjarvis9541@gmail.com>
2020-09-30 23:33:07 +0100

    ch4

--- accounts/tests.py
+++ accounts/tests.py
@@ -7,12 +7,12 @@ class CustomUserTests(TestCase):
     def test_create_user(self):
         User = get_user_model()
         user = User.objects.create_user(
-            username='michael',
-            email='michael@email.com',
-            password='test3pass2word1',
+            username='testuser',
+            email='testuser@email.com',
+            password='testpass123',
         )
-        self.assertEqual(user.username, 'michael')
-        self.assertEqual(user.email, 'michael@email.com')
+        self.assertEqual(user.username, 'testuser')
+        self.assertEqual(user.email, 'testuser@email.com')
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
@@ -20,12 +20,12 @@ class CustomUserTests(TestCase):
     def test_create_superuser(self):
         User = get_user_model()
         user = User.objects.create_superuser(
-            username='superuser',
-            email='superuser@email.com',
-            password='test3pass2word1',
+            username='testsuperuser',
+            email='testsuperuser@email.com',
+            password='testpass123',
         )
-        self.assertEqual(user.username, 'superuser')
-        self.assertEqual(user.email, 'superuser@email.com')
+        self.assertEqual(user.username, 'testsuperuser')
+        self.assertEqual(user.email, 'testsuperuser@email.com')
         self.assertTrue(user.is_active)
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
