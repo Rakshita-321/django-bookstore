b8ab61e12f36d7d8af794198f97c497a187e82d0
Michael Jarvis <mjarvis9541@gmail.com>
2020-10-06 18:10:52 +0100

    Added basic search functionality

--- books/urls.py
+++ books/urls.py
@@ -5,4 +5,5 @@ app_name = 'books'
 urlpatterns = [
     path('', views.BookListView.as_view(), name='list'),
     path('<uuid:pk>', views.BookDetailView.as_view(), name='detail'),
+    path('search/', views.SearchResultsListView.as_view(), name='search_results'),
 ]
